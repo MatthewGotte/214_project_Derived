f9ae60aaf5b802830990be32cdb7c3974352d080
MatthewGotte <matthewgotte@gmail.com>
2021-11-18 23:37:23 +0200

    dos2unix

--- System/rebuild.sh
+++ System/rebuild.sh
@@ -1,10 +1,12 @@
-jRED='\033[0;31m'
+RED='\033[0;31m'
 BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 rm -f -r filelist.txt CMakeLists.txt CMakeCache.txt cmake_install.cmake Makefile CMakeFiles main;
+
 ls *.h *.cpp > filelist.txt;
+
 touch CMakeLists.txt;
 echo 'cmake_minimum_required(VERSION 3.16)' >> CMakeLists.txt;
 echo 'project(Derived)' >> CMakeLists.txt;
@@ -15,7 +17,6 @@ less filelist.txt >> CMakeLists.txt;
 echo ')' >> CMakeLists.txt;
 echo 'target_link_libraries(main ${GTEST_LIBRARIES} pthread)' >> CMakeLists.txt;
 rm -f filelist.txt;
-
 printf "${GREEN}Built CMakeLists.txt${NC}\n";
 printf "${RED}trying => cmake CMakeLists.txt${NC}\n";
 cmake CMakeLists.txt;
@@ -23,12 +24,9 @@ printf "${BLUE}cmake CMakeLists.txt => completed.${NC}\n";
 
 echo 'run:' >> Makefile;
 echo -e '\t./main' >> Makefile;
-
 echo 'gdb:' >> Makefile;
 echo -e '\tgdb main' >> Makefile;
-
 echo 'valgrind:' >> Makefile;
 echo -e '\tvalgrind --leak-check=full ./main'  >> Makefile;
-
 echo 'rebuild:' >> Makefile;
 echo -e '\t./rebuild.sh' >> Makefile
