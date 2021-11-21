a35c9674268332613bc1dd5d6109ab6cbdfd184e
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2021-11-21 20:31:30 +0200

    Update CrewDragon.cpp

--- System/CrewDragon.cpp
+++ System/CrewDragon.cpp
@@ -1,4 +1,3 @@
-
 #include "CrewDragon.h"
 #include "SpaceCraft.h"
 #include "Human.h"
@@ -33,20 +32,20 @@ void CrewDragon::printSpaceCraft() {
     
     HumanIterator* it  = h->createHumanIterator();
     Human * human = it->first();
-    while(it->hasNext()){
-        cout<<"Hie I am " <<human->getName() <<" glad to be on board Falcon9"<<endl;
+    while (it->hasNext()) {
+        cout << "Hi I am " << human->getName() << " glad to be on board Falcon9" << endl;
         human = it->next();
     }
-    cout<<"Hie I am " <<human->getName() <<" glad to be on board Falcon9"<<endl;
+    cout << "Hi I am " << human->getName() << " glad to be on board Falcon9" << endl;
 
 
     CargoIterator* it2 = c->createCargoIterator();
     Cargo * cargo = it2->first();
-    while(it2->hasNext()){
-        cout<<"We have " <<cargo->getName() <<" on board the Falcon9"<<endl;
+    while (it2->hasNext()) {
+        cout << "We have " << cargo->getName() << " on board the Falcon9" << endl;
         cargo = it2->next();
     }
-      cout<<"We have " <<cargo->getName() <<" on board the Falcon9"<<endl;
+    cout << "We have " << cargo->getName() << " on board the Falcon9" << endl;
 
 }
 
